01b4ac795bc1017ac00526ead30e55fc2a7aeec5
Thomas Philipona <philipona@puzzle.ch>
2020-12-09 09:41:00 +0100

    Add scripts for simplicity reasons

---
+++ upgrade-webide.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+helm upgrade --namespace $1 \
+--set ide.password=$3 \
+--set ingress.hosts[0].host=webide-$1.$2 \
+--set ingress.tls[0].hosts[0]=webide-$1.$2 \
+--set ingress.tls[0].secretName=webide-$1 \
+--set ingress.hosts[0].paths[0]=/  \
+firstrel ./
